5570717a6712c55ef16ae7b6f5b73873da7f0f04
D <igothippie@yahoo.com>
2025-06-29 21:15:50 -0400

    disable token

--- server/middleware/2.auth.ts
+++ server/middleware/2.auth.ts
@@ -3,7 +3,8 @@ export default defineEventHandler((event) => {
   if (event.path === '/api/proxy-create-link') return;
 
   const token = getHeader(event, 'Authorization')?.replace(/^Bearer\s+/, '');
-  if (event.path.startsWith('/api/') && !event.path.startsWith('/api/_') && token !== useRuntimeConfig(event).siteToken) {
+//  if (event.path.startsWith('/api/') && !event.path.startsWith('/api/_') && token !== useRuntimeConfig(event).siteToken) {
+  if (false) {
     throw createError({
       status: 401,
       statusText: 'Unauthorized',
